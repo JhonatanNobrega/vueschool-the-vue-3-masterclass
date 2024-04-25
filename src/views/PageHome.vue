7f99b31732f319488aa69d7d25b9ad6d7a97b014
Jhonatan Nobrega <jhonatan_nobrega@hotmail.com>
2024-04-25 14:26:10 -0300

    Backup

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -14,21 +14,46 @@ const posts = ref<Post[]>(sourceData.posts);
 const users = ref<User[]>(sourceData.users);
 
 // Methods
-function postById(postsId: string): Post | undefined {
-  return posts.value.find((p) => p.id === postsId);
+function postById(postsId: string): Post {
+  let post = posts.value.find((p) => p.id === postsId);
+  if (post) return post;
+  throw new Error('Post not found');
 }
-function userById(userId: string | undefined): User | undefined {
-  return users.value.find((u) => u.id === userId);
+function userById(userId: string): User {
+  const user = users.value.find((u) => u.id === userId);
+  if (user) return user;
+  throw new Error('User not found');
 }
 </script>
 
 <template>
-  <div>Hello form page home</div>
-  <div v-for="thread in threads" :key="thread.id">
-    <h2>{{ thread.title }}</h2>
-    <div v-for="postId in thread.posts" :key="postId">
-      <p>{{ userById(postById(postId)?.userId)?.name }}</p>
-      <p>{{ postById(postId)?.text }}</p>
+  <div v-for="thread in threads" :key="thread.id" class="col-large push-top">
+    <h1>{{ thread.title }}</h1>
+
+    <div class="post-list">
+      <div v-for="postId in thread.posts" :key="postId" class="post">
+        <div class="user-info">
+          <a href="#" class="user-name">
+            {{ userById(postById(postId).userId).name }}
+          </a>
+
+          <a href="#">
+            <img class="avatar-large" :src="userById(postById(postId).userId).avatar" alt="" />
+          </a>
+
+          <p class="desktop-only text-small">107 posts</p>
+        </div>
+
+        <div class="post-content">
+          <div>
+            <p>{{ postById(postId).text }}</p>
+          </div>
+        </div>
+
+        <div class="post-date text-faded">
+          {{ postById(postId).publishedAt }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
